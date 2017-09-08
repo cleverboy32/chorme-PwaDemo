4aa698e17c6a0dd403046921a9a6626a3da2c0da
wuyunzhu <wuyunzhu@souche.com>
2017-09-08 11:11:07 +0800

    feat: sw 监听push

--- prod-service-worker.js
+++ prod-service-worker.js
@@ -47,4 +47,31 @@ self.addEventListener('fetch', (e) => {
         );
     }
 
-})
+});
+
+// 监听推送事件 然后显示通知
+self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: 'assets/imgs/48.png',
+        badge: 'assets/imgs/48.png'
+    };
+  
+    event.waitUntil(self.registration.showNotification(title, options));
+  });
+
+// 监听通知的点击事件
+self.addEventListener('notificationclick', function(event) {
+console.log('[Service Worker] Notification click Received.');
+
+event.notification.close();
+
+event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+);
+});
+
